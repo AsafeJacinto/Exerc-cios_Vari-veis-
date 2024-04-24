28820d00d4171e37365989dbe7c1eed5d24c4396
asafe jacinto <asafejacinto@gmail.com>
2024-04-24 20:09:25 -0300

    Quantidade_de_times

---
+++ 6Quantidade de times completos.js
@@ -0,0 +1,13 @@
+let quantidadeDePessoas
+let QuantidadeDeJogadoresEmCadaTime
+
+//quantidadeDePessoas = 23
+//QuantidadeDeJogadoresEmCadaTime = 5
+
+quantidadeDePessoas = 40
+QuantidadeDeJogadoresEmCadaTime = 4
+
+
+let banco = quantidadeDePessoas % QuantidadeDeJogadoresEmCadaTime
+
+console.log(banco);
