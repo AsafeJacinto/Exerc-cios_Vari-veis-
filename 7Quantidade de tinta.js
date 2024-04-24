7d84bd7086f140da52704113881da6a842e0006c
asafe jacinto <asafejacinto@gmail.com>
2024-04-24 20:25:55 -0300

    quantidade_de_tinta

---
+++ 7Quantidade de tinta.js
@@ -0,0 +1,16 @@
+let comprimento
+let largura
+let rendimento
+
+
+comprimento = 10
+largura = 20
+rendimento = 40
+
+
+let area = largura * comprimento
+let quantideDeTinta = area * rendimento
+
+console.log(quantideDeTinta);
+
+
