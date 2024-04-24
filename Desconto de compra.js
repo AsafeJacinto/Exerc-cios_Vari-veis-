2abbeeb22a00c18a938e0846d0afab0fe398be3f
asafe jacinto <asafejacinto@gmail.com>
2024-04-24 19:50:40 -0300

    desconto_de_compras

---
+++ Desconto de compra.js
@@ -0,0 +1,13 @@
+let preco
+let Desconto
+let valorDesconto
+
+preco = 10
+Desconto = 30
+valorDesconto = Desconto / 100 * preco
+
+
+
+let precoFinal = preco - valorDesconto
+console.log(precoFinal
+)
